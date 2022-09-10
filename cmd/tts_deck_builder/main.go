57a12ad45c1b3d88bcb7e922c3661f441761a090
HardDie <oleg1995sysoev@yandex.ru>
2022-09-11 00:42:32 +0300

    Removed old generator

--- cmd/tts_deck_builder/main.go
+++ cmd/tts_deck_builder/main.go
@@ -22,17 +22,17 @@
 package main
 
 import (
-	"flag"
-	"fmt"
 	"log"
 	"net/http"
 
 	"tts_deck_build/api"
-	generator "tts_deck_build/internal/generator_old"
 	"tts_deck_build/internal/network"
 )
 
-func WebServer() {
+func main() {
+	// Setup logs
+	log.SetFlags(log.Llongfile | log.Ltime)
+
 	log.Println("Listening on :5000...")
 
 	if false {
@@ -43,33 +43,5 @@ func WebServer() {
 	err := http.ListenAndServe(":5000", nil)
 	if err != nil {
 		log.Fatal(err.Error())
-	}
-}
-
-func main() {
-	// Setup logs
-	log.SetFlags(log.Llongfile | log.Ltime)
-
-	// Setup run flags
-	genDeckMode := flag.Bool("generate_object", false, "Run process of generating json deck object")
-	gameName := flag.String("game", "", "Title of game for generator")
-	helpMode := flag.Bool("help", false, "Show help")
-	flag.Parse()
-
-	switch {
-	case *genDeckMode:
-		generator.GenerateDeckObject(*gameName)
-	case *helpMode:
-		fmt.Println("How to use:")
-		fmt.Println("1. Build images from ${sourceDir}/*.json descriptions (-generate_image -game gameID)")
-		fmt.Println("2. Upload images on some hosting (steam cloud)")
-		fmt.Println("3. Write URL for each image in ${resultDir}/images.json file")
-		fmt.Println("4. Build deck object ${resultDir}/deck.json (-generate_object -game gameID)")
-		fmt.Println("5. Put deck object into \"Tabletop Simulator/Saves/Saved Objects\" folder")
-		fmt.Println()
-		fmt.Println("Choose one of the mode:")
-		flag.PrintDefaults()
-	default:
-		WebServer()
 	}
 }
