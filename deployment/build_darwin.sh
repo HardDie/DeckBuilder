833b79a3f4aaea1d8b39325025d04cbdbf25ecb3
HardDie <oleg1995sysoev@yandex.ru>
2022-10-12 10:47:13 +0300

    Added version for windows and darwin

--- deployment/build_darwin.sh
+++ deployment/build_darwin.sh
@@ -4,9 +4,14 @@ set -u
 set -o pipefail
 set -e
 
+BACKEND=$(git --git-dir ../.git rev-parse --short HEAD)
+FRONTEND=$(git --git-dir ../gui/.git rev-parse --short HEAD)
+
 mkdir -p out
 
-CGO_ENABLED=0 GOARCH=amd64 GOOS=darwin go build -a -o DeckBuilder_amd64 -v ../cmd/deck_builder
+CGO_ENABLED=0 GOARCH=amd64 GOOS=darwin go build -ldflags \
+	"-X main.BackendCommit=${BACKEND} -X main.FrontendCommit=${FRONTEND}" \
+	-a -o DeckBuilder_amd64 -v ../cmd/deck_builder
 mkdir tmp
 cp -r DeckBuilder.app tmp/
 rm tmp/DeckBuilder.app/Contents/MacOS/put_binary_here
@@ -16,7 +21,9 @@ tar -czf ../out/deckbuilder_darwin_amd64.tar.gz DeckBuilder.app
 cd ..
 rm -rf tmp
 
-CGO_ENABLED=0 GOARCH=arm64 GOOS=darwin go build -a -o DeckBuilder_arm64 -v ../cmd/deck_builder
+CGO_ENABLED=0 GOARCH=arm64 GOOS=darwin go build -ldflags \
+	"-X main.BackendCommit=${BACKEND} -X main.FrontendCommit=${FRONTEND}" \
+	-a -o DeckBuilder_arm64 -v ../cmd/deck_builder
 mkdir tmp
 cp -r DeckBuilder.app tmp/
 rm tmp/DeckBuilder.app/Contents/MacOS/put_binary_here
--- deployment/build_windows.sh
+++ deployment/build_windows.sh
@@ -4,13 +4,21 @@ set -u
 set -o pipefail
 set -e
 
+BACKEND=$(git --git-dir ../.git rev-parse --short HEAD)
+FRONTEND=$(git --git-dir ../gui/.git rev-parse --short HEAD)
+
 mkdir -p out
 
 cd ../cmd/deck_builder
 go generate
 
-CGO_ENABLED=0 GOARCH=amd64 GOOS=windows go build -a -o ../../deployment/DeckBuilder_amd64.exe -v .
-CGO_ENABLED=0 GOARCH=386 GOOS=windows go build -a -o ../../deployment/DeckBuilder_386.exe -v .
+CGO_ENABLED=0 GOARCH=amd64 GOOS=windows go build -ldflags \
+	"-X main.BackendCommit=${BACKEND} -X main.FrontendCommit=${FRONTEND}" \
+	-a -o ../../deployment/DeckBuilder_amd64.exe -v .
+
+CGO_ENABLED=0 GOARCH=386 GOOS=windows go build -ldflags \
+	"-X main.BackendCommit=${BACKEND} -X main.FrontendCommit=${FRONTEND}" \
+	-a -o ../../deployment/DeckBuilder_386.exe -v .
 rm resource.syso
 
 cd ../../deployment
