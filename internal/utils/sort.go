792ef61177615029e74bff601a9f943d133181a4
HardDie <oleg1995sysoev@yandex.ru>
2022-11-02 14:41:06 +0300

    Make sure that we have same items order if we sort by name

--- internal/utils/sort.go
+++ internal/utils/sort.go
@@ -16,8 +16,16 @@ func Sort[T ISortable](items *[]T, field string) {
 	sort.SliceStable(*items, func(i, j int) bool {
 		switch field {
 		default: // "name"
+			if (*items)[i].GetName() == (*items)[j].GetName() {
+				// If the names are equal, in this case order by date of creation
+				return (*items)[i].GetCreatedAt().Before((*items)[j].GetCreatedAt())
+			}
 			return (*items)[i].GetName() < (*items)[j].GetName()
 		case "name_desc":
+			if (*items)[i].GetName() == (*items)[j].GetName() {
+				// If the names are the same, then in this case the reverse order by date of creation
+				return (*items)[i].GetCreatedAt().After((*items)[j].GetCreatedAt())
+			}
 			return (*items)[i].GetName() > (*items)[j].GetName()
 		case "created":
 			return (*items)[i].GetCreatedAt().Before((*items)[j].GetCreatedAt())
